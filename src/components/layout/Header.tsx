414703a1414732107bf6a7cc351ce4485c5f2a0b
Oscar Escorcia <oe319840@gmail.com>
2025-11-04 10:05:05 -0500

    feat: added new tools to do the dashboard and add the project strucure, but, the SPA redirections is missing

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,10 @@
+import { headers } from 'next/headers';
+import react from 'react';
+
+export default function Header(){
+    return(
+        <header>
+            <h3>Welcome</h3>
+        </header>
+    );
+}
